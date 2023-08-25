66b9e741411744b6442a795590d4edef7b821784
Thales Lima <lthales53@gmail.com>
2023-08-25 16:43:35 -0300

    Adding intial configurations

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -3,23 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//HotChocolate
+builder.Services.AddGraphQLServer();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app
+.UseRouting()
+.UseEndpoints(endpoints => 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    endpoints.MapGraphQL();
+    
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
